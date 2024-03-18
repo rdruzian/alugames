ae4d0822de89772398808322415420c8c2f9f651
esoft36 - Renato Druzian <ex161676@gmail.com>
2024-03-17 23:30:30 -0300

    Validações de exceções

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -2,7 +2,7 @@ data class Jogo(
     val titulo: String,
     val capa: String
 ) {
-    var descricao = ""
+    var descricao: String? = ""
     override fun toString(): String {
         return "Jogo(titulo='$titulo', capa='$capa', descricao='$descricao')"
     }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,53 @@ import com.google.gson.Gson
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.*
 
 fun main() {
-    val client = HttpClient.newHttpClient()
-    val request = HttpRequest.newBuilder().uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146")).build()
-
-    val response = client.send(request, BodyHandlers.ofString()).body()
-
-    println(response)
-
+    //Declarações
+    var meuJogo: Jogo? = null
+    var meuInfoJogo: InfoJogo? = null
+    var request: HttpRequest? = null
     val gson = Gson()
-    val meuInfoJogo = gson.fromJson(response, InfoJogo::class.java)
 
-    val meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
+    val leitura = Scanner(System.`in`)
+    print("Digite um código de jogo para buscar: ")
+    val busca = leitura.nextLine()
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
 
-    println(meuJogo)
+    val client = HttpClient.newHttpClient()
+
+    //Requisição http
+    val resultrequest = runCatching {
+        request = HttpRequest.newBuilder().uri(URI.create(endereco)).build()
+    }
+    resultrequest.onFailure {
+        print("Erro ao buscar a url")
+    }
+    resultrequest.onSuccess {
+        val response = client.send(request, BodyHandlers.ofString()).body()
+        meuInfoJogo = gson.fromJson(response, InfoJogo::class.java)
+    }
+
+    //Cria objeto Jogo
+    val result = runCatching {
+        if(meuJogo != null) {
+            meuJogo = Jogo(meuInfoJogo!!.info.title, meuInfoJogo!!.info.thumb)
+        }
+    }
+    result.onSuccess {
+        print("Deseja inserir descrição: ")
+        val opcao = leitura.nextLine()
+        if (opcao.equals("s", true)) {
+            print("Insira a descrição: ")
+            val desc = leitura.nextLine()
+            meuJogo?.descricao = desc
+        } else {
+            meuJogo?.descricao = meuJogo!!.titulo
+        }
+        println(meuJogo)
+    }
+
 }
